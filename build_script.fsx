111e326e245b7fde40bc117b6458a7cbfd30adad
Jander IT <info@jander.it>
2013-12-01 14:41:34 +0100

    Build: nuget

--- build_script.fsx
+++ build_script.fsx
@@ -4,6 +4,13 @@ open Fake
 
 // Config
 
+let project = "Bibliothek"
+let project_description = "DDC Demo Bibliothek"
+let project_summary = "DDC Demo Bibliothek Nuget Paket"
+let project_tags = "Demo"
+let copyright = "Copyright Jander IT 2013"
+let authors = ["Jander IT"]
+
 let product_version = "0.0"
 let build_number = match buildServer with
                    | Jenkins -> jenkinsBuildNumber.PadLeft(5, '0')
@@ -12,6 +19,8 @@ let build_number = match buildServer with
 let version = sprintf "%s.%s-dev" product_version build_number
 
 let buildDir="./build"
+let packagingDir="./temp_nuget"
+let deployDir="./deploy"
 
 let nunitSearchPaths = [ProgramFilesX86 @@ "nunit 2.6.3" @@ "bin"; ProgramFilesX86 @@ "nunit 2.6.1" @@ "bin"; ProgramFilesX86 @@ "nunit 2.6.2" @@ "bin"]
 
@@ -32,7 +41,7 @@ let empty () = ()  // c#: ()=>{}
 // Targets
 
 Target "Clean" ( fun _ ->
-  CleanDirs [buildDir]
+  CleanDirs [buildDir;packagingDir;deployDir]
 )
 
 Target "BuildDebug" ( fun _ ->
@@ -55,10 +64,35 @@ Target "Test" ( fun _ ->
   !! (buildDir @@ "*.dll") |> NUnit configuration
 )
 
+Target "Nuget" ( fun _ ->
+    // Copy all the package files into a package folder
+    CreateDir packagingDir
+    CreateDir deployDir
+
+    let net45 = packagingDir @@ "lib" @@ "net45"
+    CreateDir net45
+    CopyFiles net45 [buildDir @@ project+".dll"]
+
+    NuGet (fun p -> 
+        {p with
+            Authors = authors
+            Project = project
+            Description = project_description
+            OutputPath = deployDir
+            Summary = project_summary
+            WorkingDir = packagingDir
+            Copyright = copyright
+            Version = version
+            Publish = false
+            Tags = project_tags
+             }) 
+            "template.nuspec"    
+) 
+
 Target "Default" empty
 
 // Dependencies
 
-"Clean" ==> "BuildDebug" ==> "Test" ==> "Default"
+"Clean" ==> "BuildDebug" ==> "Test" ==> "Nuget" ==> "Default"
 
 RunTargetOrDefault "Default"
