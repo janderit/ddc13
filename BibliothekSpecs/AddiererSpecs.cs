b06948f8b18e1731e6c62066a5ac853dd3397ed0
Jander IT <info@jander.it>
2013-12-01 14:02:04 +0100

    Addierer implementiert

---
+++ BibliothekSpecs/AddiererSpecs.cs
@@ -0,0 +1,17 @@
+﻿using Bibliothek;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BibliothekSpecs
+{
+    [TestFixture]
+    public class AddiererSpecs
+    {
+        [Test]
+        public void Addiert_zwei_Zahlen()
+        {
+            var subject = new Addierer();
+            subject.Addiere(22, 20).Should().Be(42);
+        }
+    }
+}
